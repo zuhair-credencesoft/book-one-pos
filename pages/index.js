fa63aa4accad7cbe655051b4fc8ffbbef71506ef
inzamam <inzamam@credencesoft.in>
2022-06-24 15:53:13 +0530

    Created Responsive Hero Section in Homepage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <div className={styles.leftSection}>
         <div className={styles.heroText}>
           <h1 className={styles.Text1}>
-            First<span className={styles.gradientText} style={{marginLeft: '15px'}}>mobile</span> POS software
+            First<span className={styles.gradientText} style={{marginLeft: '15px', marginRight: '15px'}}>mobile</span> POS software
           </h1>
           <p className={styles.Text2}>
           BookOnePOS solutions is unrivaled for providing a robust and flexible POS system that is reliable and unwavering in its performance, no matter the demands of your venue.
@@ -21,8 +21,7 @@ export default function Home() {
           <button className={styles.heroTextButton}>Take a Free demo</button>
         </div>
         <div  className={styles.polygon}>
-        <img className={styles.polygonLine} src='/Vector-954.jpg'></img>
-        <img className={styles.polygonArrow} src='/Polygon-1.jpg'></img>
+        <img className={styles.polygonArrow} src='/heroArrow.svg'></img>
       </div>
         </div>
         <div className={styles.signUpContainer}>
@@ -31,6 +30,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+          <img  className={styles.bottomGradient} src="/Ellipse-137.svg" alt="" />
     </div>
   );
 }
