b0a5fa00d7e60ca0d08b02f38da3fbe141198d93
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-27 15:57:02 +0530

    Integration Page Added.

--- pages/integration.js
+++ pages/integration.js
@@ -1,51 +1,51 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import styles from "../styles/Integration.module.css";
 export default class Integration extends React.Component {
   render() {
     return (
-      <div className="lg:container lg:mx-auto">
-        <div className="flex lg:mx-20 ">
-          <div className="lg:w-1/2 w-full">
-            <img
-              src="/integration.svg"
-              alt=""
-              className="lg:h-[40rem] lg:w-[40rem]"
-            />
+      <div>
+        <div className={styles.heroContainer}>
+          <div className={styles.signUpContainer}>
+            <div className={styles.IntegrationImg}>
+              <img src="/integration.svg" alt="" />
+            </div>
           </div>
-          <div className="lg:w-1/2 w-full">
-            <p className="text-8xl font-bold mt-32  text-center">
-              Lorem
-              <span
-                className={styles.gradientText}
-                style={{ marginLeft: "15px", marginRight: "15px" }}
-              >
-                Ipsum
-              </span>
-            </p>
-            <p className="mt-8 text-center text-2xl font-['Open_Sans']">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+          <div className={styles.leftSection}>
+            <div className={styles.heroText}>
+              <h1 className={styles.Text1}>
+                Lorem
+                <span
+                  className={styles.gradientText}
+                  style={{ marginLeft: "15px", marginRight: "15px" }}
+                >
+                  Ipsum
+                </span>{" "}
+              </h1>
+              <p className={styles.Text2}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
           </div>
         </div>
         <div>
-          <p className="font-semibold text-center text-7xl ">
-            Online Order Integrations
-          </p>
+          <h3 className={styles.onlineHeading}>Online Order Integrations</h3>
         </div>
-        <div className="container px-20 mt-10">
-          <img src="/swiggyZomato.svg" alt="" />
+        <div className={styles.onlineContainer}>
+          <div className={styles.integrationImg}>
+            <img src="/swiggyZomato.svg" alt="" />
+          </div>
         </div>
-        <div className="mt-20">
-          <p className="font-semibold text-center text-7xl ">
-            Payment Integrations
-          </p>
+        <div>
+          <h3 className={styles.paytmHeading}>Payment Integrations</h3>
         </div>
-        <div className="container px-20 mt-10">
-          <img src="/googlepay.svg" alt="" />
+        <div className={styles.onlineContainer}>
+          <div className={styles.integrationImg}>
+            <img src="/googlepay.svg" alt="" />
+          </div>
         </div>
       </div>
     );
