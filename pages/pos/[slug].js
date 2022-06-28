d2c6abb376760f5080a41f9996e1b32cad5094a2
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-28 12:27:06 +0530

    Responsive Styles Added.

--- pages/pos/[slug].js
+++ pages/pos/[slug].js
@@ -8,42 +8,46 @@ const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
   let pageData = {};
-    PosData.forEach(element => {
-      if (element[slug]) {
-        pageData = element[slug];
-      }
-    });
+  PosData.forEach((element) => {
+    if (element[slug]) {
+      pageData = element[slug];
+    }
+  });
 
   return (
     <div className={styles.container}>
       <div className={styles.sectionOne}>
         <h5>{pageData.heading}</h5>
-        <p>
-          {pageData.description}
-        </p>
-        {pageData.listItems && <ul>
-          <li>
-            {pageData.listItems.listItem1}
-          </li>
-          <li>
-            {pageData.listItems.listItem2}
-          </li>
-          {pageData.listItems.listItem3?<li>
-            {pageData.listItems.listItem3}
-          </li>:''}
-          {pageData.listItems.listItem4?<li>
-            {pageData.listItems.listItem4}
-          </li>:''}
-        </ul>}
+        <p>{pageData.description}</p>
+        {pageData.listItems && (
+          <ul>
+            <li>{pageData.listItems.listItem1}</li>
+            <li>{pageData.listItems.listItem2}</li>
+            {pageData.listItems.listItem3 ? (
+              <li>{pageData.listItems.listItem3}</li>
+            ) : (
+              ""
+            )}
+            {pageData.listItems.listItem4 ? (
+              <li>{pageData.listItems.listItem4}</li>
+            ) : (
+              ""
+            )}
+          </ul>
+        )}
       </div>
       <div className={styles.sectionTwo}>
-        {pageData.image?<Image
-          src={pageData.image}
-          alt=""
-          height={600}
-          width={600}
-          layout="intrinsic"
-        />:''}
+        {pageData.image ? (
+          <img
+            src={pageData.image}
+            alt=""
+            // height={500}
+            // width={500}
+            // layout="intrinsic"
+          />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
